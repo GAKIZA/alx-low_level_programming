e0ba4a2dc332f93c7b119a4879e342e0c662a317
GAKIZA <imansamgis@gmail.com>
2023-03-18 04:39:47 -0700

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Printing _alphabet
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int a;
+
+a = 0;
+while
+	( a < 10) {
+		printf("%d", a);
+		a++;
+	}
+putchar('\n');
+return (0);
+}
