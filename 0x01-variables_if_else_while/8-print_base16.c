f3896194fc84e7f659ee9ba5b199176d0b1c50bc
GAKIZA <imansamgis@gmail.com>
2023-03-19 14:19:32 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Printing _Base 16
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+char a;
+int d;
+
+a = 'z';
+d = 0;
+while
+	(d < 10) {
+		putchar(d + '0');
+		d++;
+	}
+while
+	(a <= 'f') {
+		putchar(a);
+		a++;
+}
+putchar('\n');
+return (0);
+}
