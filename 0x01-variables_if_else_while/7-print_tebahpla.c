02992301abeeb897d1c09c66fe7894cf3b1f1de2
GAKIZA <imansamgis@gmail.com>
2023-03-19 13:51:18 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Printing _alphabet
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+char a;
+
+a = 'z';
+while
+        (a >= 'a') {
+                putchar(a);
+                a--;
+        }
+putchar('\n');
+return (0);
+}
