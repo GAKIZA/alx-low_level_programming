b2612a549dd6f7353bbdc0898e9af5b28b49c6cf
GAKIZA <imansamgis@gmail.com>
2023-03-22 03:49:41 -0700

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+/**
+ * main - creating theprogram to list first 100 fibonacci numbers
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	int i = 0;
+	long j = 1, k = 2;
+	while(i < 100)
+	{
+		if (i == 0)
+			printf("%ld",j);
+		else if (i == 1)
+			printf(", %ld", k);
+		else
+		{
+			k += j;
+			j = k-j;
+			printf(", %ld", k);
+		}
+		++i;
+	}
+		printf("\n");
+		return (0);
+	}
+
+	
