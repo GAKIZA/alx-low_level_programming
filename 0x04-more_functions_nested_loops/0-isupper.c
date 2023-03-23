57b8cafa27457330a72a93bb77303c0ffec506c5
GAKIZA <imansamgis@gmail.com>
2023-03-23 15:37:51 -0700

    0-isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,16 +1,17 @@
 #include "main.h"
 /**
- * main - Program to check whether letter is upper or lower
+ * isupper - Program to check whether letter is upper or lower
+ * @x: number to be checked
  * Return: 1 upper 0 lower
  */
 int isupper(int x)
 {
-        if (x >= 65 && x <= 90)
-        {
-                return (1);
-        }
-        else
-        {
-                return (0);
-        }
+	if (x >= 65 && x <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
