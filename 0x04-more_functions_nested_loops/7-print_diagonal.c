b0450d773dd3fce6cf593824f78f8533e92ecd76
GAKIZA <imansamgis@gmail.com>
2023-03-24 05:05:07 -0700

    7-print_diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ *print_diagonal - Print diagonal line of n segment
+ *@n: Number of segment of a diagonal line
+ *Return: empty
+ */
+
+void print_diagonal(int n)
+{
+	int x, y;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 0; x < n; x++)
+		{
+			for (y = 0; y < x; y++)
+			{
+				_putchar(92);
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+}
