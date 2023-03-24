2d8f81943e5c8619502e5b9dfec47530ea20734e
GAKIZA <imansamgis@gmail.com>
2023-03-23 17:03:36 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_numbers - Print numbers om 0 to 9 followed by a line
+ * Return: numbers from 0 to 9
+ */
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+		_putchar (a + '0');
+	_putchar('\n');
+}
