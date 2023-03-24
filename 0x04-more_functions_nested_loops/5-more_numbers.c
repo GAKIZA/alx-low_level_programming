7303bed8778023ac2183700e8590e6eb865dce82
GAKIZA <imansamgis@gmail.com>
2023-03-23 19:46:37 -0700

    5-more_numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * more_numbers - Printing numbers from 0 to 14 ten times
+ * Return: a series of numbers from 0 to 14 ten times
+ */
+
+void more_numbers(void)
+{
+	int i, k;
+
+	for (i = 0; i <= 10; i++)
+	{
+	for (k = 0; k <= 14; k++)
+	{
+	if (k < 10)
+	{
+		_putchar(k + '0');
+	}
+	else
+	{
+		_putchar('1');
+		_putchar(k % 10 + '0');
+	}
+	}
+
+	_putchar('\n');
+	}
+}
+
+
